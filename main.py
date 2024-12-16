51edd73741fe2de44e825e9870258d0e40a61f4c
choihoyeon1214 <82034643+choihoyeon1214@users.noreply.github.com>
2024-12-16 23:12:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from PIL import Image
 from PIL import ImageFilter
 import numpy as np
 img = Image.open('test.jpg')
-def jcr():
-    x, y = img.size
-    img_wb = Image.open('test.jpg').convert("L")
-    img_blur = img_wb.filter(ImageFilter.GaussianBlur(-10))
-#def
-
+x, y = img.size
+img_wb = Image.open('test.jpg').convert("L")
+img_blur = img_wb.filter(ImageFilter.GaussianBlur(-10))
+img.show()
+img_wb.show()
+img_blur.show()
