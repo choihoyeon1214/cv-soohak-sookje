355f2ef2a2e270c02002ad1d18c1f35b590f7d84
choihoyeon1214 <82034643+choihoyeon1214@users.noreply.github.com>
2024-12-17 08:10:35 +0900

    Add files via upload

---
+++ edge.py
@@ -0,0 +1,25 @@
+import cv2
+from PIL import Image
+
+
+input_path = 'sanz.png' 
+image = cv2.imread(input_path, cv2.IMREAD_GRAYSCALE) 
+
+blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+
+
+low_threshold = 50  
+high_threshold = 150  
+edges = cv2.Canny(blurred_image, low_threshold, high_threshold)
+
+
+edges_pillow = Image.fromarray(edges)
+
+
+edges_pillow.show()
+
+
+output_path = 'edges_output.jpg'
+edges_pillow.save(output_path)
+
+print(f"Canny Edge Detection 결과가 저장되었습니다: {output_path}")
