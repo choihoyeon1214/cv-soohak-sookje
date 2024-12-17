015916530123b3174a91e07dd1e4c2ec074aee34
choihoyeon1214 <82034643+choihoyeon1214@users.noreply.github.com>
2024-12-17 23:11:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ x, y = img.size
 img_wb = Image.open('test.jpg').convert("L")
 img_blur = img_wb.filter(ImageFilter.GaussianBlur(-10))
 img.show()
-img_wb.show()
+#img_wb.show()
 img_blur.show()
+#pip install opencv-contrib-python opencv-contrib-python-headless opencv-python opencv-python-headless
